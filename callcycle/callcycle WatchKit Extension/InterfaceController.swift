aa6dbc4bd9aa92122a65349033a5516cc3aa4946
KevinBebel <boygenius@MacBook-Pro.local>
2015-04-10 11:36:12 +0200

    New Commit

---
+++ callcycle/callcycle WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  callcycle WatchKit Extension
+//
+//  Created by Boy Genius on 2015/04/10.
+//  Copyright (c) 2015 Boy Genius. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ callcycle/callcycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  callcycle
+//
+//  Created by Boy Genius on 2015/04/10.
+//  Copyright (c) 2015 Boy Genius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
